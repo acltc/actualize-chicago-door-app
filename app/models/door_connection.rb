5afb2e80799998718e0032a448ee430a68dc1e8a
Peter Jang <peter.x.jang@gmail.com>
2020-04-08 17:18:55 -0500

    Use credentials for quad relay configuration

--- app/models/door_connection.rb
+++ app/models/door_connection.rb
@@ -4,10 +4,10 @@ class DoorConnection
   def self.open_door_and_elevator
     begin
       puts "=== open_door_and_elevator"
-      # uri = URI.parse("http://#{ENV["QIPADDRESS"]}:#{ENV["QPORT"]}/stateFull.xml?relay1state=1&relay2state=1")
+      # uri = URI.parse("http://#{Rails.application.credentials.quadrelay[:QIPADDRESS]}:#{Rails.application.credentials.quadrelay[:QPORT]}/stateFull.xml?relay1state=1&relay2state=1")
       # http = Net::HTTP.new(uri.host, uri.port)
       # request = Net::HTTP::Get.new(uri.request_uri)
-      # request.basic_auth(ENV["QUSER"], ENV["QPWORD"])
+      # request.basic_auth(Rails.application.credentials.quadrelay[:QUSER], Rails.application.credentials.quadrelay[:QPWORD])
       # response = http.request(request)
       # xml_doc = Nokogiri::XML(response.body)
       # xml_doc.xpath("//relay1state").text == "1" && xml_doc.xpath("//relay2state").text == "1"
@@ -20,10 +20,10 @@ class DoorConnection
   end
 
   def self.close_door
-    # uri = URI.parse("http://#{ENV["QIPADDRESS"]}:#{ENV["QPORT"]}/stateFull.xml?relay1state=0")
+    # uri = URI.parse("http://#{Rails.application.credentials.quadrelay[:QIPADDRESS]}:#{Rails.application.credentials.quadrelay[:QPORT]}/stateFull.xml?relay1state=0")
     # http = Net::HTTP.new(uri.host, uri.port)
     # request = Net::HTTP::Get.new(uri.request_uri)
-    # request.basic_auth(ENV["QUSER"], ENV["QPWORD"])
+    # request.basic_auth(Rails.application.credentials.quadrelay[:QUSER], Rails.application.credentials.quadrelay[:QPWORD])
     # response = http.request(request)
     # xml_doc = Nokogiri::XML(response.body)
     # xml_doc.xpath("//relay1state").text == "0"
@@ -31,10 +31,10 @@ class DoorConnection
   end
 
   def self.close_elevator
-    # uri = URI.parse("http://#{ENV["QIPADDRESS"]}:#{ENV["QPORT"]}/stateFull.xml?relay2state=0")
+    # uri = URI.parse("http://#{Rails.application.credentials.quadrelay[:QIPADDRESS]}:#{Rails.application.credentials.quadrelay[:QPORT]}/stateFull.xml?relay2state=0")
     # http = Net::HTTP.new(uri.host, uri.port)
     # request = Net::HTTP::Get.new(uri.request_uri)
-    # request.basic_auth(ENV["QUSER"], ENV["QPWORD"])
+    # request.basic_auth(Rails.application.credentials.quadrelay[:QUSER], Rails.application.credentials.quadrelay[:QPWORD])
     # response = http.request(request)
     # xml_doc = Nokogiri::XML(response.body)
     # xml_doc.xpath("//relay2state").text == "0"
