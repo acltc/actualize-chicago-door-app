261c2f15d4fa23ebeebd6819604a39d1bc1a9993
Peter Jang <peter.x.jang@gmail.com>
2020-04-09 11:32:16 -0500

    Add conditional navbar links with styling fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :logged_in_as_admin?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in_as_admin?
+    current_user && current_user.is_admin?
+  end
+
+  def authenticate_admin!
+    redirect_to "/" unless logged_in_as_admin?
   end
 end
